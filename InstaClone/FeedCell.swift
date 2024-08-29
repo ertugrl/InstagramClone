97f370b1616dcbdfdc67fb0f77069e903b0f9abf
Ertuğrul Şahin <ertugrul.2003sahin@gmail.com>
2024-08-29 16:36:36 +0300

    setPost function has added instead of transferring documentID with the label component

--- InstaClone/FeedCell.swift
+++ InstaClone/FeedCell.swift
@@ -10,35 +10,38 @@ import FirebaseFirestore
 
 class FeedCell: UITableViewCell {
 
+    // MARK: UI Elements
     @IBOutlet weak var postImageView: UIImageView!
-    
     @IBOutlet weak var userEmailLabel: UILabel!
-    
     @IBOutlet weak var commentLabel: UILabel!
-    
     @IBOutlet weak var likeLabel: UILabel!
     
-    @IBOutlet weak var documentIDLabel: UILabel!
+    private var documentID: String?
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func setPost(_ post: Post) {
+        postImageView.sd_setImage(with: URL(string: post.imageUrl))
+        userEmailLabel.text = post.userEmail
+        commentLabel.text = post.comment
+        likeLabel.text = String(post.likes)
+        documentID = post.documentID
     }
     
     @IBAction func likeButtonClicked(_ sender: UIButton) {
-        let firestoreDatabase = Firestore.firestore()
         
-        if let likeCount = Int(likeLabel.text!) {
-            let likeStore = ["likes" : likeCount + 1] as [String : Any]
-            firestoreDatabase.collection("Posts").document(documentIDLabel.text!).setData(likeStore, merge: true)
+        guard let likeText = likeLabel.text, let likeCount = Int(likeText),
+              let documentID = documentID else {
+//            UIAlertController.showAlert(on: self, title: "Error", message: "Unable to retrieve like count or document ID.")
+            return
         }
+        
+        let likeStore = ["likes": likeCount + 1] as [String : Any]
+        Firestore.firestore().collection("Posts").document(documentID).setData(likeStore, merge: true)
+        
+        // BEFORE THE CHANGES
+//        let firestoreDatabase = Firestore.firestore()
+//        if let likeCount = Int(likeLabel.text!) {
+//            let likeStore = ["likes" : likeCount + 1] as [String : Any]
+//            firestoreDatabase.collection("Posts").document(documentIDLabel.text!).setData(likeStore, merge: true)
+//        }
     }
-    
-
 }
